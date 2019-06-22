1e7d90750f00683595ec9cb08efb08a81a8807e9
miladmemarian <milad.memarian@gmail.com>
2019-06-21 17:38:47 -0700

    Validation added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-router.get('/', (req, res) => res.send('users route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
+    check(
+      'password',
+      'Please eneter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('users route');
+  }
+);
 
 module.exports = router;
